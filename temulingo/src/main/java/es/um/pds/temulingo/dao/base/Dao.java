cc16bb41b58d767dc5d9b40bee1375a99ca42ac3
Hugo Sánchez <hugosanchezmartinez4@gmail.com>
2025-06-11 23:50:40 +0200

    fix: DAO actualizado

--- temulingo/src/main/java/es/um/pds/temulingo/dao/base/Dao.java
+++ temulingo/src/main/java/es/um/pds/temulingo/dao/base/Dao.java
@@ -5,13 +5,13 @@ import java.util.Optional;
 
 public interface Dao<T> {
 
-	void edit(T obj);
+	T edit(T obj);
 
 	Optional<T> get(long id);
 
 	List<T> getAll();
 
-	void save(T t);
+	T save(T t);
 
 	void delete(T t);
 }
