3608f73368caeb67e85052c8d2d9257a2f415864
Hugo Sánchez <hugosanchezmartinez4@gmail.com>
2025-06-22 21:27:35 +0200

    feat(ui): actualizada la barra de menú

--- temulingo/src/main/java/es/um/pds/temulingo/config/ConfiguracionTemulingo.java
+++ temulingo/src/main/java/es/um/pds/temulingo/config/ConfiguracionTemulingo.java
@@ -19,7 +19,7 @@ public class ConfiguracionTemulingo {
 	public static final String AUTOR = "Facultad de Informática - Universidad de Murcia";
 	public static final String ANO = "2025";
 
-	public static final String URL = "https://github.com/hsanchezm7/learning-app";
+	public static final String URL = "https://github.com/hsanchezm7/temulingo";
 
 	public static final String FORMATO_FECHA_HORA = "dd-MM-yyyy HH:mm:ss";
 	public static final String PERSISTENCE_UNIT_NAME = "temulingo-persistence-unit";
